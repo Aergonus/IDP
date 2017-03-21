027876786f8ff3fafa9dacde5b8970add2625a2c
Spellchaser <KangqiaoLei@gmail.com>
2017-03-21 13:41:09 -0400

    refactor

--- js/setup.js
+++ js/setup.js
@@ -30,9 +30,46 @@ var moourls = [];
 var returnedMoos = [];
 var winmoo;
 
+setup();
+
 // Setup Libraries, Acquire Locks, and Load Assets
 function setup() {
+	// Instantiate Threejs loader which sets up and inits AudioContext
+	var loader = new THREE.AudioLoader();
+	
+	// Array of Sounds to Load
+	for (var i = 0; i <= 10; i++) {
+		moourls.push('./media/sounds/'+i+'.mp3');
+	}
+	moourls.push('./media/sounds/win.mp3');
+	
+	moourls.forEach(function(listItem, index){
+		// Load sound
+		loader.load(
+			// Resource URL
+			listItem,
+			// Function when resource is loaded
+			function ( audioBuffer ) {
+				// Success Handler from Ajax call
+				returnedMoos[index] = audioBuffer; // Save audioBuffer response
+				// See if we're done with the last ajax call
+				--ajaxCallsRemaining;
+				info.innerHTML = "Loading Progress " + ((totalCalls - ajaxCallsRemaining)/totalCalls*100) + " %";
+				if (ajaxCallsRemaining <= 0) {
+					// All resources loaded! Unlock Game
+					unlock();
+				}
+			},
+			// Function called when download progresses
+			onProgress,
+			// Function called when download errors
+			onError
+		);
+	});
+}
 
+// Runs once after page is loaded
+function unlock() {
 	blocker = document.getElementById( 'blocker' );
 	instructions = document.getElementById( 'instructions' );
 
@@ -65,42 +102,19 @@ function setup() {
 
 	};
 
-	// Instantiate Threejs loader which sets up and inits AudioContext
-	var loader = new THREE.AudioLoader();
+	// Hook pointer lock state change events
+	document.addEventListener("pause", lockchange, false );
 	
-	// Array of Sounds to Load
-	for (var i = 0; i <= 10; i++) {
-		moourls.push('./media/sounds/'+i+'.mp3');
-	}
-	moourls.push('./media/sounds/win.mp3');
+	instructions.addEventListener( 'click', function ( event ) {
+		instructions.style.display = 'none'; 
+		
+		// Unlock Pause
+		pause = false;
+		document.dispatchEvent(pauseEvent);
+	}, false );
 	
-	moourls.forEach(function(listItem, index){
-		// Load sound
-		loader.load(
-			// Resource URL
-			listItem,
-			// Function when resource is loaded
-			function ( audioBuffer ) {
-				// Success Handler from Ajax call
-				returnedMoos[index] = audioBuffer; // Save audioBuffer response
-				// See if we're done with the last ajax call
-				--ajaxCallsRemaining;
-				info.innerHTML = "Loading Progress " + ((totalCalls - ajaxCallsRemaining)/totalCalls*100) + " %";
-				if (ajaxCallsRemaining <= 0) {
-					// All resources loaded! Unlock Game
-					// Hook pointer lock state change events
-					document.addEventListener("pause", lockchange, false );
-					
-					instructions.addEventListener( 'click', function ( event ) {
-						instructions.style.display = 'none'; 
-						
-						// Unlock Pause
-						pause = false;
-						document.dispatchEvent(pauseEvent);
-					}, false );
-					
-					info.innerHTML = "Resources Loaded, click to begin.";
-					
+	info.innerHTML = "Resources Loaded, click to begin.";
+	
 sound = new THREE.PositionalAudio( listener );
 sound.setBuffer( returnedMoos[0] );
 sound.setRefDistance( 10 );
@@ -109,22 +123,12 @@ sound.setDistanceModel('exponential');
 sound.setLoop(true);
 sound.play();
 
-// events
+	// events
 
-addEventListeners();
+	addEventListeners();
 
-animate();
-				}
-			},
-			// Function called when download progresses
-			onProgress,
-			// Function called when download errors
-			onError
-		);
-	});
+	animate();
 }
-
-setup();
 
 function animate() {
 	//if ( controlsEnabled ) { animateCamera( delta ); }
