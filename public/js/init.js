5d30622492d07799f9ad5903fcdbaa9e10b9f7a0
Spellchaser <KangqiaoLei@gmail.com>
2017-04-28 00:04:32 -0400

    Final working game

--- public/js/init.js
+++ public/js/init.js
@@ -34,17 +34,39 @@ var database = firebase.database();
 var info = document.getElementById( 'info' );
 var blue = document.getElementById( 'blue' );
 var red = document.getElementById( 'red' );
+var offers = document.getElementById( 'offers' );
+var leftoffer = document.getElementById( 'leftoffer' );
+var rightoffer = document.getElementById( 'rightoffer' );
 
 var playerRef = database.ref('players');
 
 // TODO: Listener for blue/red updates to update UI
 var blueRef = database.ref('players/blue');
 var redRef = database.ref('players/red');
+var voted = false;
 blueRef.on('value', function(snapshot) {
-  console.log(snapshot.val());
+	var blue = snapshot.val();
+	if (document.body.style.backgroundColor = '#CCCCFF' 
+	 && blue.uid != firebase.auth().currentUser.uid) {
+		document.body.style.backgroundColor = '#000';
+		info.innerHTML = "You are not a player. <br /> Click the buttons to acquire control.";
+		offers.style.display = "none";
+		leftoffer.innerHTML = "";
+		rightoffer.innerHTML = "";
+		voted = false;
+	}
 });
 redRef.on('value', function(snapshot) {
-  console.log(snapshot.val());
+	var red = snapshot.val();
+	if (document.body.style.backgroundColor = '#FFCCCC' 
+	 && red.uid != firebase.auth().currentUser.uid) {
+		document.body.style.backgroundColor = '#000';
+		info.innerHTML = "You are not a player. <br /> Click the buttons to acquire control.";
+		offers.style.display = "none";
+		leftoffer.innerHTML = "";
+		rightoffer.innerHTML = "";
+		voted = false;
+	}
 });
 
 var gameData;
@@ -80,6 +102,9 @@ gameRef.on('value', function(snapshot) {
 
 var pendingRef = database.ref('secret');
 
+var updateGame = function(){
+gameRef.once('value').then(function(snapshot) {
+	gameData = snapshot.val();
 	// Not sure if this is a race condition, so it's safer to enclose this inside here
 	pendingRef.on('value', function(snapshot) {
 	  console.log(snapshot.val());
@@ -111,6 +136,11 @@ var pendingRef = database.ref('secret');
 		updated: false
 	  });
 	});
+});
+console.log("Hello");
+}
+
+updateGame();
 
 var secretRef; 
 function take(color){
@@ -119,6 +149,7 @@ function take(color){
 		timestamp: firebase.database.ServerValue.TIMESTAMP
 	}).then(function(success) {
 		secretRef = database.ref("secret/"+color);
+		offers.style.display = "";
 		info.innerHTML = "You are " + color + " Player.";
 		red.innerHTML = "Score: " + gameData.red.score + " Last Move: " + (gameData.red.move ? "Cooperate" : "Defect");
 		blue.innerHTML = "Score: " + gameData.blue.score + " Last Move: " + (gameData.blue.move ? "Cooperate" : "Defect");
@@ -132,18 +163,27 @@ function take(color){
 		}
 	}).catch(function(error) {
 		// Notify user of failure
-		info.innerHTML = "Failed to take over " + color + " Player.";
+		info.innerHTML = "Unable to be the new " + color + " Player.";
 		console.log(error.message);
 	});
 }
 
-
 function guess(pchoice){
+	if (voted){
+		return;
+	}
+	if (pchoice){
+		rightoffer.innerHTML = "&#10003";
+		voted = true;
+	} else {
+		leftoffer.innerHTML = "X";
+		voted = true;
+	}
 	secretRef.set({
 		uid: firebase.auth().currentUser.uid,
 		choice: pchoice
 	}).then(function(success) {
-		console.log("Updated Choice");
+		updateGame();
 	}).catch(function(error) {
 		console.log("Failed to update");
 	});
