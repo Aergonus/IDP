bf3614436d1b126485331457d3db43dba9bfffbd
Spellchaser <KangqiaoLei@gmail.com>
2017-04-27 21:41:24 -0400

    cleanup

--- js/archive/includeLibraries.js
+++
@@ -1,7 +0,0 @@
-			<script type="text/javascript" src="./js/Libraries/three.min.js"></script>
-			<script type="text/javascript" src="./js/Libraries/stats.min.js"></script>
-			<script type="text/javascript" src="./js/Libraries/Detector.js"></script>
-			<script type="text/javascript" src="./js/Libraries/MouseControls.js"></script>
-			<script type="text/javascript" src="./js/Libraries/FlyControls.js"></script>
-			<script type="text/javascript" src="./js/Libraries/FirstPersonControls.js"></script>
-			<script type="text/javascript" src="./js/Libraries/PointerLockControls.js"></script>
